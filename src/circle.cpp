bb6292037345fe7f6b2bf578912e1428cc3c9b3a
Alexander <87474596+kobych@users.noreply.github.com>
2025-03-09 20:06:14 +0300

    Update circle.cpp

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,50 @@
 // Copyright 2022 UNN-CS
 #include <cstdint>
 #include "circle.h"
+#include <cmath>
 
+Circle::Circle(double r) {
+    setRadius(r);
+}
+
+void Circle::setRadius(double r) {
+    radius = r;
+    updateFerence();
+    updateArea();
+}
+
+void Circle::setFerence(double f) {
+    ference = f;
+    updateRadius();
+    updateArea();
+}
+
+void Circle::setArea(double a) {
+    area = a;
+    updateRadius();
+    updateFerence();
+}
+
+double Circle::getRadius() const {
+    return radius;
+}
+
+double Circle::getFerence() const {
+    return ference;
+}
+
+double Circle::getArea() const {
+    return area;
+}
+
+void Circle::updateFerence() {
+    ference = 2 * M_PI * radius;
+}
+
+void Circle::updateArea() {
+    area = M_PI * radius * radius;
+}
+
+void Circle::updateRadius() {
+    radius = ference / (2 * M_PI);
+}
